d81f5e2b0a3c1216b2e57e026598c0003fd2da46
Por2505 <s6035512061@phuket.psu.ac.th>
2020-04-09 00:59:01 +0700

    page

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Input from './components/Input';
 function App() {
   return (
     <div className="App">
-      <h1>LOSEPET</h1>
-        <Input/>
+      
+        <center><Input/></center>
     </div>
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -21,28 +21,43 @@ export default () => {
       //await uploadToBackend('endpoint', {image: image.raw}, config)
     }
     return (
+      <div>
+      <header id="header">
+			LosePet
+				
+			</header>
+
+		
+      <div class="head">
         <div class="container">
-  <form>
-  <input
+            <form>
+            <div class="row">
+              <div class="col-50">
+                <input
                 type="radio"
                 name="lost"/>Lose Pet
-            <input
+                </div>
+              <div class="col-50">
+               <input
                 type="radio"
                 name="lost"/>Found Pet
+                </div>
+      
+           </div>
                 <br/>
     <div class="row">
     <div class="col-25">
-      <label for="name">Name</label>
+        <label for="name">Name</label>
     </div>
-    <div class="col-75">
-      <input type="text" id="name" name="name" placeholder="Pet name.."/>
+    <div class="col-50">
+       <input type="text" id="name" name="name" placeholder="Pet name.."/>
     </div>
     </div>
     <div class="row">
     <div class="col-25">
       <label for="name">Photo</label>
     </div>
-    <div class="col-75">
+    <div class="col-50">
      <label htmlFor="upload-button">
         {
           image.preview ? <img src={ image.preview } width="300" height="300" /> : (
@@ -60,16 +75,27 @@ export default () => {
  </div>
  <div class="row">
     <div class="col-25">
+      <label for="area">Location</label>
+    </div>
+    <div class="col-50">
+      <input type="text" id="area" name="area" placeholder="location"/>
+    </div>
+    </div>
+ <div class="row">
+    <div class="col-25">
       <label for="phone">Phone</label>
     </div>
-    <div class="col-75">
+    <div class="col-50">
       <input type="text" id="phone" name="phone" placeholder="Your Phone"/>
     </div>
     </div>
+   
   <div class="row">
     <input type="submit" value="Submit"/>
   </div>
   </form>
+</div>
+</div>
 </div>
     )
     }
